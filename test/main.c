1ab73db906e9e0e414a59cbc00e73138ea745618
SeveralAttempts <ss_s0l0@mail.ru>
2026-01-13 10:09:06 +0300

    task diff two arrays incomplete

--- test/main.c
+++ test/main.c
@@ -1,12 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char *argv[])
-{
-    if (argc == 2)
-    {
-        printf("What is %s?\n", argv[1]);
-    }
-    printf("%d\n", EXIT_SUCCESS);
+void difference_between_two_arrays() {
+    int f_l, s_l = 0;
+    char len_buf[14];
+    fgets(len_buf, sizeof(len_buf), stdin);
+    sscanf(len_buf, "%d %d", &f_l, &s_l);
+    const int f = f_l * 2;
+    const int s = s_l * 2;
+    char f_buf[f];
+    char s_buf[s];
+    fgets(f_buf, sizeof(f_buf), stdin);
+    fgets(s_buf, sizeof(s_buf), stdin);
+    
+}
+
+int main(int argc, char *argv[]) {
+    
     return EXIT_SUCCESS;
 }
